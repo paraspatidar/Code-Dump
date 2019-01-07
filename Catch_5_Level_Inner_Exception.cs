f86baeb1670f1892c197194002652d25887ccf26
Paras Patidar <paraspatidar@users.noreply.github.com>
2019-01-07 12:20:29 +0530

    updated with proper changes and intercept

--- Catch_5_Level_Inner_Exception.cs
+++ Catch_5_Level_Inner_Exception.cs
@@ -1,22 +1,27 @@
-catch(Exception ex)
-            {
-                data.responce = " Exception : LEVEL 1: " + Environment.NewLine + ex.Message;
-                if(ex.InnerException !=null)
+//lets create and extension method wiht class
+public static class Extensions
+    {
+        public static string DeepExceptiopn(this Exception ex)
+        {
+            string result = string.Empty;
+            result = " Exception : LEVEL 1: " + Environment.NewLine + ex.Message+ "::::Trace===>"+ex.StackTrace;
+            if (ex.InnerException != null)
                 {
-                    data.responce = data.responce + Environment.NewLine + "LEVEL 2:" + Environment.NewLine+ ex.InnerException.Message;
-                    if(ex.InnerException.InnerException!=null)
+                    result = result + Environment.NewLine + "LEVEL 2:" + Environment.NewLine + ex.InnerException.Message;
+                    if (ex.InnerException.InnerException != null)
                     {
-                        data.responce = data.responce + Environment.NewLine + "LEVEL 3:" + Environment.NewLine + ex.InnerException.InnerException.Message;
+                        result = result + Environment.NewLine + "LEVEL 3:" + Environment.NewLine + ex.InnerException.InnerException.Message;
 
                         if (ex.InnerException.InnerException.InnerException != null)
                         {
-                            data.responce = data.responce + Environment.NewLine + "LEVEL 4:" + Environment.NewLine + ex.InnerException.InnerException.InnerException.Message;
+                            result = result + Environment.NewLine + "LEVEL 4:" + Environment.NewLine + ex.InnerException.InnerException.InnerException.Message;
                             if (ex.InnerException.InnerException.InnerException.InnerException != null)
                             {
-                                data.responce = data.responce + Environment.NewLine + "LEVEL 5:" + Environment.NewLine + ex.InnerException.InnerException.InnerException.InnerException.Message;
+                                result = result + Environment.NewLine + "LEVEL 5:" + Environment.NewLine + ex.InnerException.InnerException.InnerException.InnerException.Message;
                             }
                         }
                     }
                 }
-                
-            }
+            return result;
+        }
+    }
