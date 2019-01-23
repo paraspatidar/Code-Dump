57c570a4fd960e78fd965849bea01adb71f6ebf8
Paras Patidar <paraspatidar@users.noreply.github.com>
2019-01-23 20:48:59 +0530

    updated instance method

--- Catch_5_Level_Inner_Exception.cs
+++ Catch_5_Level_Inner_Exception.cs
@@ -25,3 +25,44 @@ public static class Extensions
             return result;
         }
     }
+
+
+//and then in You catch block call it by :
+      catch(Exception ex)
+		 {
+		   return ex.DeepException();
+		 }
+
+
+
+/////IF You  Dont want static Implementation and extension method and just want as normal method , then use it in this way in existing class:
+
+  public  string DeepException(Exception ex)
+        {
+            string result = string.Empty;
+            result = " Exception : LEVEL 1: " + ex.Message+ "::::Trace===>"+ex.StackTrace;
+            if (ex.InnerException != null)
+                {
+                    result = result +  "LEVEL 2:" +  ex.InnerException.Message;
+                    if (ex.InnerException.InnerException != null)
+                    {
+                        result = result  + "LEVEL 3:" + ex.InnerException.InnerException.Message;
+
+                        if (ex.InnerException.InnerException.InnerException != null)
+                        {
+                            result = result +  "LEVEL 4:" +  ex.InnerException.InnerException.InnerException.Message;
+                            if (ex.InnerException.InnerException.InnerException.InnerException != null)
+                            {
+                                result = result +  "LEVEL 5:" + ex.InnerException.InnerException.InnerException.InnerException.Message;
+                            }
+                        }
+                    }
+                }
+            return result;
+        }
+
+//and then in You catch block call it by :
+      catch(Exception ex)
+		 {
+		   return "Error in Method Internal  code !!!!!!!!!!!! "+DeepException(ex);
+		 }
